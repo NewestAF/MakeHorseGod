75a92b0a230104d9a9596d65c5f9af1f7dca019b
NewestAF <castle5314@naver.com>
2022-05-01 11:39:35 +0900

    Initial commit

---
+++ src/main/java/com/newestaf/makehorsegod/MakeHorseGod.java
@@ -0,0 +1,17 @@
+package com.newestaf.makehorsegod;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MakeHorseGod extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/newestaf/makehorsegod/events/EnchantEventManager.java
@@ -0,0 +1,14 @@
+package com.newestaf.makehorsegod.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+
+public class EnchantEventManager implements Listener {
+    @EventHandler
+    public void onItemEnchant(PrepareItemEnchantEvent event) {
+
+    }
+
+
+}
